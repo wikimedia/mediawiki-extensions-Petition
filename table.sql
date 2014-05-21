e4668cac8634be7d900407d4cae430a83e139bd6
Pcoombe <pcoombe@wikimedia.org>
2014-05-21 15:05:30 +0100

    Fix table setup to support SQLite
    SQLite requires pt_id to be set as primary key when first defined
    in order for auto_increment to work
    https://sqlite.org/faq.html#q1
    
    Bug: 65488
    Change-Id: I747f0f27ee68c8587bb359f588793e2dcdf665c2

--- table.sql
+++ table.sql
@@ -1,6 +1,6 @@
 -- Adds tables for petition
 CREATE TABLE /*_*/petition_data (
-	pt_id int unsigned auto_increment,
+	pt_id int unsigned PRIMARY KEY auto_increment,
 	pt_petitionname varchar(255),
 	pt_pagetitle varchar(255),
 	pt_source varchar(255),
@@ -9,8 +9,7 @@ CREATE TABLE /*_*/petition_data (
 	pt_country varchar(2),
 	pt_message blob,
 	pt_share boolean,
-	pt_timestamp varbinary(14),
-	PRIMARY KEY (pt_id)
+	pt_timestamp varbinary(14)
 ) /*$wgDBTableOptions*/;
 
 CREATE INDEX /*i*/pt_petitionname ON /*_*/petition_data (pt_petitionname);
