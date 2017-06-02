500f0ef3ec16b585476ea7e9447c49bc08ed1e57
Umherirrender <umherirrender_de.wp@web.de>
2017-06-02 23:05:37 +0200

    Add phpcs and make pass
    Change-Id: I1c55f273826304228f5cfabffbb0a01b3cf26863

--- SpecialPetition.php
+++ SpecialPetition.php
@@ -30,26 +30,26 @@ class SpecialPetition extends IncludableSpecialPage {
 
 		$countries = SpecialPetition::getCountryArray( $this->getLanguage()->getCode() );
 		$form = SpecialPetition::defineForm( $petitionName, $source, $countries );
-		$form->setSubmitCallback( array( $this, 'petitionSubmit' ) );
+		$form->setSubmitCallback( [ $this, 'petitionSubmit' ] );
 
 		$form->prepareForm();
 
 		$result = $form->tryAuthorizedSubmit();
 
 		if ( $result === true || ( $result instanceof Status && $result->isGood() ) ) {
-			$htmlOut = '<span class="petition-done">' . wfMessage('petition-done')->text() . '</span>';
+			$htmlOut = '<span class="petition-done">' . wfMessage( 'petition-done' )->text() . '</span>';
 		} else {
 			$htmlOut = '<div class="petition-form">' . "\n";
 			$numberOfSignatures = SpecialPetition::getNumberOfSignatures( $petitionName );
 			$htmlOut .= '<div id="petition-num-signatures">';
-			$htmlOut .= wfMessage('petition-num-signatures', $numberOfSignatures)->escaped();
+			$htmlOut .= wfMessage( 'petition-num-signatures', $numberOfSignatures )->escaped();
 			$htmlOut .= '</div>' . "\n";
 			// Add the form, with any errors if there was an attempted submission
-			$htmlOut .= $form->getHtml($result) . "\n";
+			$htmlOut .= $form->getHtml( $result ) . "\n";
 			$htmlOut .= '</div>' . "\n";
 		}
 
-		$out->addHtml($htmlOut);
+		$out->addHtml( $htmlOut );
 
 	}
 
@@ -64,15 +64,15 @@ class SpecialPetition extends IncludableSpecialPage {
 		global $wgPetitionDatabase;
 
 		if ( $this->getUser()->pingLimiter( 'edit' ) ) {
-			return wfMessage('actionthrottledtext')->text();
+			return wfMessage( 'actionthrottledtext' )->text();
 		}
 
-		$dbw = wfGetDB( DB_MASTER, array(), $wgPetitionDatabase );
+		$dbw = wfGetDB( DB_MASTER, [], $wgPetitionDatabase );
 		if ( $dbw->isReadOnly() ) {
 			throw new ReadOnlyError();
 		}
 
-		$dbw->insert( 'petition_data', array(
+		$dbw->insert( 'petition_data', [
 				'pt_petitionname' => $formData['petitionname'],
 				'pt_source'       => $formData['source'],
 				'pt_name'         => $formData['name'],
@@ -81,8 +81,9 @@ class SpecialPetition extends IncludableSpecialPage {
 				'pt_message'      => $formData['personalmessage'],
 				'pt_share'        => $formData['share'],
 				'pt_timestamp'    => $dbw->timestamp()
-				),
-			__METHOD__ );
+			],
+			__METHOD__
+		);
 
 		// Update the cached number of signatures
 		$cache = ObjectCache::getMainWANInstance();
@@ -93,9 +94,9 @@ class SpecialPetition extends IncludableSpecialPage {
 		$entry = new ManualLogEntry( 'petition', 'sign' );
 		$entry->setPerformer( $this->getUser() );
 		$entry->setTarget( SpecialPage::getTitleFor( 'Petition' ) );
-		$entry->setParameters( array(
+		$entry->setParameters( [
 			'4::petitionname' => $formData['petitionname']
-		) );
+		] );
 		$entry->insert();
 
 		// And if CheckUser is installed, give it a heads up
@@ -126,10 +127,10 @@ class SpecialPetition extends IncludableSpecialPage {
 				$dbr = wfGetDB( DB_SLAVE );
 				return $dbr->selectField( 'petition_data',
 					'count(pt_id)',
-					array( 'pt_petitionname' => $petitionName )
+					[ 'pt_petitionname' => $petitionName ]
 				);
 			},
-			array( 'checkKeys' => array( $key ), 'lockTSE' => 10 )
+			[ 'checkKeys' => [ $key ], 'lockTSE' => 10 ]
 		);
 	}
 
@@ -141,7 +142,7 @@ class SpecialPetition extends IncludableSpecialPage {
 	 * @throws Exception
 	 */
 	static function getCountryArray( $language ) {
-		if ( is_callable( array( 'CountryNames', 'getNames' ) ) ) {
+		if ( is_callable( [ 'CountryNames', 'getNames' ] ) ) {
 			// Need to flip as HTMLForm requires display name as the key
 			$countries = array_flip( CountryNames::getNames( $language ) );
 			ksort( $countries );
@@ -152,47 +153,47 @@ class SpecialPetition extends IncludableSpecialPage {
 	}
 
 	static function defineForm( $petitionName, $source, $countries ) {
-		$formDescriptor = array(
-			'petitionname' => array(
+		$formDescriptor = [
+			'petitionname' => [
 				'type' => 'hidden',
 				'default' => $petitionName,
-			),
-			'source' => array(
+			],
+			'source' => [
 				'type' => 'hidden',
 				'default' => $source,
-			),
-			'name' => array(
+			],
+			'name' => [
 				'type' => 'text',
 				'label-message' => 'petition-form-name',
 				'required' => true,
-			),
-			'email' => array(
+			],
+			'email' => [
 				'type' => 'text',
 				'label-message' => 'petition-form-email',
 				'required' => true,
-			),
-			'country' => array(
+			],
+			'country' => [
 				'type' => 'select',
 				'label-message' => 'petition-form-country',
 				'options' => $countries,
-			),
-			'personalmessage' => array(
+			],
+			'personalmessage' => [
 				'type' => 'textarea',
 				'label-message' => 'petition-form-message',
 				'rows' => 4,
-			),
-			'share' => array(
+			],
+			'share' => [
 				'type' => 'check',
 				'hidelabel' => true, // otherwise get an extra empty <label> element
-				'label-raw' => wfMessage('petition-form-share')->parse(),
+				'label-raw' => wfMessage( 'petition-form-share' )->parse(),
 				'cssclass'  => 'plainlinks',
-			),
-			'privacy' => array(
+			],
+			'privacy' => [
 				'type' => 'info',
-				'default' => wfMessage('petition-form-privacy')->parse(),
+				'default' => wfMessage( 'petition-form-privacy' )->parse(),
 				'raw' => true,
-			),
-		);
+			],
+		];
 
 		$form = HTMLForm::factory( 'vform', $formDescriptor, RequestContext::getMain(), 'petition' );
 		$form->setId( 'petition-form' );
